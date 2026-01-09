5da82f50d9ac48822ce6f15646888bfcafeb6ba5
Nikhil Sah <106432581+Sah-Nikhil@users.noreply.github.com>
2026-01-09 13:36:56 +0530

    Depreciating formulae page temporarily

--- app/formulae/page.tsx
+++ app/formulae/page.tsx
@@ -6,12 +6,12 @@ import Footer from "@/components/ui/footer-modern";
 export default function FormulaePage() {
 	return (
 		<div className="bg-white min-h-screen text-black font-sans selection:bg-blue-500/30">
-			<Navbar />
+			{/* <Navbar />
 			<MobileNav />
 			<main className="min-h-screen flex items-center justify-center">
 				<h1 className="text-2xl">Formulae</h1>
 			</main>
-			<Footer />
-		</div>
+			<Footer /> */}
+        </div>
 	);
 }
